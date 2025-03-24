fc6eb166089e96eaa110a2787e3c492453a88b76
odun23ayo <mariamadekunle883@gmail.com>
2025-03-24 14:10:30 +0100

    update

--- script.js
+++ script.js
@@ -4,13 +4,17 @@ function searchFunction(params) {
 }
 let lastScrollY = window.scrollY;
 window.addEventListener("scroll",function() {
-    let nav = document.getElementById("navbar");
+    let nav = 
+    document.getElementById("navbar")
+    ;
+    if (!nav) return;
     if (Window.scrollY > 50) {
         nav.classList.add("scrolled"); 
     } else {
         nav.classList.remove("scrolled");
     }
-    if(window.scrollY > lastScrollY) {
+    if(window.scrollY > 
+        lastScrollY) {
          nav.classList.add("hidden")
         } else{
             nav.classList.remove("hidden")
